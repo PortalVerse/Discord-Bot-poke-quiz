194f8dc37648fd675e16d5f80789f76ba952d044
Ssparkii <crispijnw@gmail.com>
2016-11-01 12:39:13 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 var discord = require("discord.js");
 var bot = new discord.Client();
-var util = require('util');
 
 bot.on("message", msg =>{
-	util.inspect(msg.guild.roles, {depth: 1});
 	let prefix = ";";
+	let myRole = msg.guild.roles.get("242949350664175616");
 	
 	if(msg.author.bot) return;
 	
-	// if(bot.member.roles.has(botbosses.id)){
+	if(msg.member.hasRole("242949350664175616")){
 	
 		if (msg.content.startsWith(prefix + "ping")) {
 		msg.channel.sendMessage("pong!");
 		
-	}
+		}
 	
 	}
+	else{
+		msg.channel.sendMessage("You don\'t have the right role.")
+	}
 	
-	// else{
-		// msg.channel.sendMessage("You don\'t have the right role.")
-	// }
-	
-);
+});
 
 bot.login("MjQyNjA2OTMwNjEzMzcwODgw.CvjZiQ.sioME_mmVnUumvkYrVRnxnB4yeg");
