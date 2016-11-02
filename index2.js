7650e311cfaefa5140647e6aaf5c83a5287359a8
Dirk B <dirk.brouwer58@gmail.com>
2016-11-02 16:13:17 +0100

    Data from JSON file properly formatted

--- index2.js
+++ index2.js
@@ -1,6 +1,8 @@
 var discord = require("discord.js");
-var XMLHttpRequest = require("local-xmlhttprequest").XMLHttpRequest;
+// var XMLHttpRequest = require("local-xmlhttprequest").XMLHttpRequest;
+var fs = require("fs");
 var bot = new discord.Client();
+const database = require("./test.json");
 
 bot.on("ready", () => {
 	bot.user.setGame('with hentai');
@@ -27,31 +29,40 @@ bot.on("message", msg =>{
 		
 		if(msg.content.startsWith(prefix + "pq")) {
 			
-			var xhr = new XMLHttpRequest();
+			// var xhr = new XMLHttpRequest();
 			
-			console.log("unsent", xhr.status); //test
+			// console.log("unsent", xhr.status); //test
 			
-			xhr.open("GET", "test.json", true);
+			// xhr.open("GET", "test.json", true);
 			
-			xhr.onprogress = function(){
-				console.log("LOADING" , xhr.status); //test
-			};
+			// xhr.onprogress = function(){
+				// console.log("LOADING" , xhr.status); //test
+			// };
 			
-			xhr.onload = function(){
-				console.log("DONE", xhr.status); //test
-			};
+			// xhr.onload = function(){
+				// console.log("DONE", xhr.status); //test
+			// };
 			
-			if (xhr.readyState == XMLHttpRequest.DONE){
-				msg.channel.sendMessage(xhr.responseText);
-				console.log("succes", xhr.status ) //test
-			} else{
-				console.log("Fail");
-			}
-			console.log("open", xhr.status) //test
+			// if (xhr.readyState == XMLHttpRequest.DONE){
+				// textchannel.sendMessage(JSON.parse(xhr.responseText));
+				// console.log("succes", xhr.status ) //test
+			// } else{
+				// console.log("Fail");
+			// }
+			// console.log("open", xhr.status) //test
 			
-			xhr.send();
-		}
+			// xhr.send(null);
+		// }
 		
+		// fs.readFile('test.json', function(err, data)
+		// {
+			// if(err)
+			// {
+				// return console.error(err);
+			// }
+			console.log("Asynchronous read: " + database.Trivia.question);
+			msg.channel.sendMessage((database.Trivia.question));
+		};		
 });
 
 bot.login("MjQyNjA2OTMwNjEzMzcwODgw.CvjZiQ.sioME_mmVnUumvkYrVRnxnB4yeg");
