9b3a234cf76461750b992f13efcb677e7e901c15
Dirk B <dirk.brouwer58@gmail.com>
2016-11-03 21:46:35 +0100

    Changed prefix and added help command

--- index2.js
+++ index2.js
@@ -1,28 +1,32 @@
 var discord = require("discord.js");
 // var XMLHttpRequest = require("local-xmlhttprequest").XMLHttpRequest;
-var fs = require("fs");
+// var fs = require("fs");
 var bot = new discord.Client();
 const database = require("./test.json");
 const BotConfig = require("./BotConfig.json");
 
-var answer;
 var dbanswer;
 
 bot.on("ready", () => {
-	bot.user.setGame('Something');
 	console.log("Working");
+	bot.user.setGame('type /help for all commands');
 });
 
 bot.on("message", msg =>{
-	let prefix = ";";
+	let prefix = "=";
 	
 	if(msg.author.bot) return;
-
+		
+		if(msg.content.startsWith(prefix + "help"))
+		{
+			msg.author.sendMessage("List of commands:\n=ping: bot responds with pong\n=avatar: gives url of your discord avatar\n=pq: starts trivia game\n=hentai: Just don't plz");
+		}
+		
 		if(msg.content.startsWith(prefix + "avatar")) {
 			msg.reply(msg.author.avatarURL);
 		};
 		
-		if(msg.content.startsWith(prefix + "command")) {
+		if(msg.content.startsWith(prefix + "hentai")) {
 			if(msg.author.id === "224841610410721290"){
 				msg.channel.sendFile("thing.gif");
 			}
@@ -56,23 +60,23 @@ bot.on("message", msg =>{
 			// console.log("open", xhr.status) //test
 			
 			// xhr.send(null);
-			// }
+		// }
 		
-			// fs.readFile('test.json', function(err, data)
-			// {
+		// fs.readFile('test.json', function(err, data)
+		// {
 			// if(err)
 			// {
 				// return console.error(err);
 			// }
 			msg.channel.sendMessage("Question: " + database.trivia[1].question);
-			dbanswer = database.trivia[1].answer;			
-		};
+			dbanswer = database.trivia[1].answer;		
+		};		
 		
 		var input = msg.content.toUpperCase();
 		
 		if(input === dbanswer.toUpperCase()){
-				msg.channel.sendMessage("Correct");
-				dbanswer = ""
+			msg.channel.sendMessage("Correct");
+			dbanswer = "";
 		};	
 });
 
