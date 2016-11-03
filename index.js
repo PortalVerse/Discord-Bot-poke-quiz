871ff4eacb425ec3efa7be5a94bf4ef87679a521
Dirk B <dirk.brouwer58@gmail.com>
2016-11-03 21:53:11 +0100

    Changed prefix in index.js and added secure token storage to index.js

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 var discord = require("discord.js");
 var bot = new discord.Client();
+const BotConfig = require("./BotConfig.json");
+
+bot.on("ready", () => {
+	console.log("Working");
+	bot.user.setGame('type /help for all commands');
+});
 
 bot.on("message", msg =>{
-	let prefix = ";";
+	let prefix = "=";
 	
 	if(msg.author.bot) return;
 	
-	if (msg.content.startsWith(prefix + "ping")) {
+		if (msg.content.startsWith(prefix + "ping")) {
 			
-		if(msg.member.roles.has("242949350664175616")){
-			msg.channel.sendMessage("Pong!");				
+			if(msg.member.roles.has("242949350664175616")){
+				msg.channel.sendMessage("Pong!");				
+			}
+			else{
+				msg.channel.sendMessage("You don't have the right role.");			
+			}
 		}
-		else{
-			msg.channel.sendMessage("You don\'t have the right role.");			
-		}
-	}
 });
 
-bot.login("MjQyNjA2OTMwNjEzMzcwODgw.CvjZiQ.sioME_mmVnUumvkYrVRnxnB4yeg");
+bot.login(BotConfig.login.token);
